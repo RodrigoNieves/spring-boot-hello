233f931f08c9271536a606ed5390f38a34511097
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-06 20:28:41 -0800

    Change to use Bean

--- src/main/java/rrsn/me/springboothelloworld/SpringBootHelloworldApplication.java
+++ src/main/java/rrsn/me/springboothelloworld/SpringBootHelloworldApplication.java
@@ -2,14 +2,41 @@ package rrsn.me.springboothelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import rrsn.me.springboothelloworld.servlet.HelloFilter;
+import rrsn.me.springboothelloworld.servlet.HelloListener;
+import rrsn.me.springboothelloworld.servlet.HelloServlet;
 
 @SpringBootApplication
-@ServletComponentScan
 public class SpringBootHelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloworldApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean getServletRegistrationBean() {
+		ServletRegistrationBean servletBean = new ServletRegistrationBean(new HelloServlet());
+		servletBean.addUrlMappings("/helloServlet");
+		return servletBean;
+	}
+
+	/// Register Filter.
+	@Bean
+	public FilterRegistrationBean getFilterRegistrationBean() {
+		FilterRegistrationBean filterBean = new FilterRegistrationBean(new HelloFilter());
+		// Add filter path
+		filterBean.addUrlPatterns("/helloServlet");
+		return filterBean;
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<HelloListener> getServletListenerRegistrationBean() {
+		ServletListenerRegistrationBean listenerBean =
+				new ServletListenerRegistrationBean(new HelloListener());
+		return listenerBean;
+	}
 }
